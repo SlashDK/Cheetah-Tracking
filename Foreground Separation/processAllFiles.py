cd286cb75ba5a7fb49ba73897d4e6e09a605d766
Dhruv Khurana <dhruv.k@live.com>
2018-04-09 23:38:08 -0400

    Code creating new directory for output files

--- Foreground Separation/processAllFiles.py
+++ Foreground Separation/processAllFiles.py
@@ -9,6 +9,15 @@ for root, directories, filenames in os.walk('D:\Projects\Cheetah Tracking\Data')
         fn = (os.path.join(root, filename))
         if(fn[-1] == '4'):
             filelist.append(fn)
+
+        loc = root.find("Data")
+        newpath = root[:loc] + "Data2" + root[loc + 4:]
+        
+        # print(newpath)
+
+        if not os.path.exists(newpath):
+            os.makedirs(newpath)
+a = input("asd")
 # print(filelist)
 fgbg = cv2.bgsegm.createBackgroundSubtractorMOG()
 fgbg2 = cv2.createBackgroundSubtractorMOG2(varThreshold=100)
@@ -16,13 +25,13 @@ fgbg3 = cv2.createBackgroundSubtractorKNN(
     dist2Threshold=500, detectShadows=False)
 kernel = np.ones((5, 5), np.uint8)
 kernel2 = np.ones((2, 2), np.uint8)
-fourcc = cv2.VideoWriter_fourcc(*'MJPG')
+fourcc = cv2.VideoWriter_fourcc(*'H264')
 for i in filelist:
     cap = cv2.VideoCapture(i)
     loc = i.find("Data")
-    outputDirectory = i[:loc] + "Data2" + "\output.mp4"
+    outputDirectory = i[:loc] + "Data2" + i[loc + 4:]
     out = cv2.VideoWriter(outputDirectory, fourcc, 90.0, (1920, 1080))
-    print(outasdputDirectory)
+    print(outputDirectory)
     while(1):
 
         ret, frame = cap.read()
